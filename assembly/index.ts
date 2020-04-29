bbb209e7b02f6030cfd17b0cb06e2fdcebd4a6c4
Rogelio <molekilla@gmail.com>
2020-04-29 18:19:04 -0500

    asbind

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,9 +1,31 @@
-const fs = require("fs")
-const loader = require("@assemblyscript/loader")
-const myImports = { /* ... */ }
-const cryptoxide = loader.instantiateSync(
-  fs.readFileSync("cryptoxide.wasm"),
-  myImports
-)
+import { AsBind } from "as-bind";
 
-export default cryptoxide;
+const fs = require("fs")
+const { WASI } = require("@wasmer/wasi")
+const nodeBindings = require("@wasmer/wasi/lib/bindings/node")
+
+
+// Instantiate a new WASI Instance
+let wasi = new WASI({
+    args: ['./cryptoxide.wasm'],
+    env: {},
+    bindings: {
+        ...(nodeBindings.default || nodeBindings),
+        fs: fs
+    }
+})
+
+
+const wasm = fetch("./cryptoxide.wasm");
+let wasmBytes = new Uint8Array(fs.readFileSync('./cryptoxide.wasm')).buffer
+
+const def  = wasi.getImports(new WebAssembly.Module(wasmBytes))
+// Instantiate the wasm file, and pass in our importObject
+const asBindInstance =  AsBind.instantiateSync(wasm, {
+    cryptoxide: {
+        ...def
+    }
+});
+
+
+export const cryptoxide = asBindInstance.exports.cryptoxide;
